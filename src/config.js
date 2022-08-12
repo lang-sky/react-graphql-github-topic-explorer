50d23a1cc016d4f096ae221e3d583644122f3816
lang-sky <langsky121@gmail.com>
2022-08-12 06:40:28 -0500

    apollo set header

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const GITHUB_API_TOKEN = process.env.REACT_APP_GITHUB_API_TOKEN;
-export const GRAPHQL_ENDPOINT = 'https://api.github.com/graphql';
+export const GRAPHQL_URI = 'https://api.github.com/graphql';
--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GRAPHQL_URI, GITHUB_API_TOKEN } from './config';
+
+const httpLink = createHttpLink({
+  uri: GRAPHQL_URI,
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = GITHUB_API_TOKEN;
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  uri: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
